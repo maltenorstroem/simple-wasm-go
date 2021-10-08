cfa72b40c6faffa8cb8711e83b4130d1c0186281
maltenorstroem <malte.norstroem@gmail.com>
2021-10-08 10:05:58 +0200

    chore: aim of Poc added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple WASM with Go
 
+> The aim of this PoC is that we outsource a dynamic HTML template to Go and integrate it via a web component.
+
 ## Build
 ```
 GOOS=js GOARCH=wasm go build -o app.wasm
--- main.go
+++ main.go
@@ -10,12 +10,18 @@ func add(this js.Value, p []js.Value) interface{} {
 	return js.ValueOf(sum)
 }
 
+func getTemplate(this js.Value, p []js.Value) interface{} {
+	tmpl := "button"
+	return js.ValueOf(tmpl)
+}
+
 func main() {
 	fmt.Println("Hello, WASM World!")
 
 	c := make(chan struct{}, 0)
 
 	js.Global().Set("sum", js.FuncOf(add))
+	js.Global().Set("getTmpl", js.FuncOf(getTemplate))
 
 	<-c
 
